c5c7a49a49b407c3111f62eaf8e8f7691f56580b
temuriee <critstoggle@gmail.com>
2025-06-29 23:01:00 +0400

    feature

---
+++ script.js
@@ -0,0 +1,6 @@
+let url = `https://api.github.com/users/${myName}`;
+
+let myName = document.querySelector("#gitInput");
+
+let myButn = document.querySelector("#gitButton");
+let myBtn = addEventListener("gitButton");
