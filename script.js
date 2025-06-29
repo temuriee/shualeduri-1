9562d4a6e6a5466a83152580bdbb29281e7fa4b2
temuriee <critstoggle@gmail.com>
2025-06-29 23:09:53 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let url = `https://api.github.com/users/${myName}`;
 
-let myName = document.querySelector("#gitInput");
+let myName = document.querySelector("#gitInput").innerHTML;
 
 let myButn = document.querySelector("#gitButton");
-let myBtn = addEventListener("gitButton");
+let myBtn = addEventListener("gitButton").innerHTML(url);
